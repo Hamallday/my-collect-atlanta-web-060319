fba5cf897dafb03fd0fd84e3fa96fa99b20238ed
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-31 23:59:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(array)
   i = 0
   new_array = []
-  while i < array.length
-    new_array.push( yield array[i])
-    i = i + 1
+  while 
+    i < array.length
+    new_array << yield array[i]
+    i += 1
   end
 new_array
   end
