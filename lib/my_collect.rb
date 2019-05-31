65cda0f0be49e11b0d921f119d657a23c2363e7a
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-31 23:51:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  empty_array = []
+  while i < array.length
 
+    yield(array[i])
+    i = i + 1
+  end
+  my_collect(empty_array) do |x|
+    i += 1
+  end
+  
+end
 
