87b1c81893d2a0a4d5da1f33c5126a657cac68f0
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 00:06:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,5 @@
-#
-#
-# def my_collect(array)
-#   i = 0
-#   new_array = []
-#   while
-#     i < array.length
-#     new_array.push(yield array[i])
-#     i += 1
-#   end
-# new_array
-# end
-#
+
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-# my_collect(array) do |i|
-#   name.split(" ").first
-# end
 
 def my_collect(array)
   i = 0
